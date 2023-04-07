48b44de6ff4b7515184b27893adce8c60f0a9f13
Mert Demir <101717064+mertdemirtez@users.noreply.github.com>
2023-04-07 16:06:00 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,380 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Mert Demir</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/favicon.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style1.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+  <!-- ======= Header ======= -->
+  <header id="header">         
+    <div class="container">
+
+      <h1><a href="index.html">Mert Demir</a></h1>
+      <h2><span>Founder of NÖHÜ Cyber Security Student Club</span></h2>
+      <h2><span>Jr. Full Stack Developer</span></h2>
+      <h2><span>Jr. Cyber Security Analyst</span></h2>
+      <h2><span>Computer Engineering Student</span></h2>
+      
+
+      <nav id="navbar" class="navbar">
+        <ul>
+          <li><a class="nav-link active" href="#header">Home</a></li>
+          <li><a class="nav-link" href="#about">About</a></li>
+          <li><a class="nav-link" href="#portfolio">Portfolio</a></li>
+          <li><a class="nav-link" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <div class="social-links">
+        <a href="www.instagram.com/mertdemirofficial" class="instagram"><i class="bi bi-instagram"></i></a>
+        <a href="www.linkedin.com/mertdemirtez" class="linkedin"><i class="bi bi-linkedin"></i></a>
+        <a href="www.linkedin.com/in/mertdemirtez" class="github"><i class="bi bi-github"></i></a>
+        <a href="#" class="blog"><i class="bi bi-code-slash"></i></a>
+      </div>
+
+    </div>
+  </header><!-- End Header -->
+
+  <!-- ======= About Section ======= -->
+  <section id="about" class="about">
+
+    <!-- ======= About Me ======= -->
+    <div class="about-me container">
+
+      <div class="section-title">
+        <h2>About</h2>
+        <p>Learn more about me</p>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-4" data-aos="fade-right">
+          <img src="assets/img/me.jpg" class="img-fluid" alt="">
+        </div>
+        <div class="col-lg-8 pt-4 pt-lg-0 content" data-aos="fade-left">
+          <h3>Software Developer & Cyber Security Analyst</h3>
+          <p class="fst-italic">
+            Hi, I'm Mert Demir. I was born in 2001 in Istanbul.My first encounter with the software industry was with a Web-based MMORPG game, the first game I made in the summer of 2012.Later on I made a few small games and mini Apps needed.After taking a long break, I started my studies again after winning the Computer Engineering Department in the summer of 2021.
+            Currently in the Front-end section;
+            In HTML/CSS/JAVASCRİPT, Back-end; I am working with C++/C#/JAVA languages.My goal in the coming years is to be a good cybersecurity worker.
+          </p>
+          <div class="row">
+            <div class="col-lg-6">
+              <ul>
+                <li><i class="bi bi-chevron-right"></i> <strong>Birthday:</strong> <span>21 March 2001</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Website:</strong><a href="www.mertdemirtez.com">www.mertdemirtez.com</a></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Phone:</strong> <span>NULL</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>City:</strong> <span>Ankara, Türkiye</span></li>
+              </ul>
+            </div>
+            <div class="col-lg-6">
+              <ul>
+                <li><i class="bi bi-chevron-right"></i> <strong>Age:</strong> <span>22</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Degree:</strong> <span>Bachelor's Degree</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>E-mail:</strong> <span>mertdemirtez@gmail.com</span></li>
+                <li><i class="bi bi-chevron-right"></i> <strong>Freelance:</strong> <span>Available</span></li>
+              </ul>
+            </div>
+          </div>
+          <p>
+            Currently, together with my teammates, we are preparing for the most comprehensive competition of our country, "TÜBİTAK".
+        </div>
+      </div>
+
+    </div><!-- End About Me -->
+    
+    <!-- ======= Skills  ======= -->
+    <div class="skills container">
+
+      <div class="section-title">
+        <h2>Skills</h2>
+      </div>
+
+      <div class="row skills-content">
+
+        <div class="col-lg-6">
+
+          <div class="progress">
+            <span class="skill">HTML <i class="val">80%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+          <div class="progress">
+            <span class="skill">CSS <i class="val">65%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="65" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+          <div class="progress">
+            <span class="skill">JavaScript <i class="val">25%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+          <div class="progress">
+            <span class="skill">PHP <i class="val">50%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+          <div class="progress">
+            <span class="skill">MYSQL <i class="val">75%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+        </div>
+
+        <div class="col-lg-6">
+
+          <div class="progress">
+            <span class="skill">Python <i class="val">80%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+          <div class="progress">
+            <span class="skill">C++ <i class="val">50%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+          <div class="progress">
+            <span class="skill">C# <i class="val">50%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+          <div class="progress">
+            <span class="skill">JAVA <i class="val">50%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+
+          <div class="progress">
+            <span class="skill">Cyber Security <i class="val">60%</i></span>
+            <div class="progress-bar-wrap">
+              <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100"></div>
+            </div>
+          </div>
+          
+        </div>
+
+      </div>
+
+    </div><!-- End Skills -->
+
+    <!-- ======= Interests ======= -->
+    <div class="interests container">
+
+      <div class="section-title">
+        <h2>Interests</h2>
+      </div>
+
+      <div class="row">
+        <div class="col-lg-3 col-md-4">
+          <div class="icon-box">
+            <i class="bi-music-note-list" style="color: #15ff00;"></i>
+            <h3>Listen to Music</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+          <div class="icon-box">
+            <i class="ri-game-line" style="color: #ffbb2c;"></i>
+            <h3>Playing Video Games</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 mt-4 mt-md-0">
+          <div class="icon-box">
+            <i class="bi-airplane-fill" style="color: #02e1ff;"></i>
+            <h3>Traveling to other City</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 mt-4 mt-lg-0">
+          <div class="icon-box">
+            <i class="ri-book-read-line" style="color: #ff0000;"></i>
+            <h3>Reading Books</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 mt-4">
+          <div class="icon-box">
+            <i class="ri-anchor-line" style="color: #094c80;"></i>
+            <h3>Fishing</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 mt-4">
+          <div class="icon-box">
+            <i class="bi-flower2" style="color: #f700ff;"></i>
+            <h3>Floristry</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 mt-4">
+          <div class="icon-box">
+            <i class="bi-eye-fill" style="color: #000000;"></i>
+            <h3>Watching a Football Match</h3>
+          </div>
+        </div>
+        <div class="col-lg-3 col-md-4 mt-4">
+          <div class="icon-box">
+            <i class="bi-pencil-fill" style="color: #ff8f33;"></i>
+            <h3>Work on my job</h3>
+          </div>
+        </div>
+       
+      </div>
+
+    </div><!-- End Interests -->
+
+  </section><!-- End About Section -->
+
+  <!-- ======= Resume Section ======= -->
+ <!-- End Resume Section -->
+
+  <!-- ======= Services Section ======= -->
+<!-- End Services Section -->
+
+  <!-- ======= Portfolio Section ======= -->
+  <?php require_once 'header.php';
+
+?>
+
+ <title><?php echo $ayarcek['baslik'] ?></title>
+
+  <section id="portfolio" class="portfolio">
+    <div class="container">
+
+      <div class="section-title">
+        
+        <h2>Portfolio</h2>
+        <p>My Works</p>
+      </div>
+
+      <div class="row portfolio-container">
+        
+     
+   <?php 
+   $blogsor=$baglanti->prepare("SELECT * FROM postlar ");
+   $blogsor->execute(array(
+
+   ));
+
+   $say=$blogsor->rowCount();
+   while ($blogcek=$blogsor->fetch(PDO::FETCH_ASSOC)) { ?>
+
+     <div class="col-lg-4 col-md-6 portfolio-item filter-web">
+     <div class="portfolio-wrap">
+     
+     <img style=" height:285px"src="Kuzikeri/resimler/blogs/<?php echo $blogcek['post_resim'] ?>" alt="">
+     <div class="portfolio-info">
+         
+        
+
+        <a href="https://github.com/mertdemirtez"><h4 style="color: white;"><?php echo $blogcek['post_baslik'] ?> </h4></a>
+
+      </div>
+
+    </div>
+  </div>
+
+<?php } ?>
+
+</div>
+</div>
+
+</div>
+
+
+  </section><!-- End Portfolio Section -->
+
+  <!-- ======= Contact Section ======= -->
+  <section id="contact" class="contact">
+    <div class="container">
+
+      <div class="section-title">
+        <h2>Contact</h2>
+        <p>Contact Me</p>
+      </div>
+
+      <head> 
+        <title>Mert Demir</title>
+        <link rel="icon" href="https://raw.githubusercontent.com/joelpjoji/Portfolio-v1/master/image/favicon.ico">
+        <script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.4.2/jquery.min.js"></script> 
+        <link rel="stylesheet" type="text/css" href="assets/css/index1.css">
+      </head> 
+      <body> 
+        <script type="text/javascript" src="index.js"> 
+        </script> 
+        <div id="console"></div> 
+        <div class="cursor"></div>
+    <script>
+        const cursor = document.querySelector('.cursor');
+        document.addEventListener('mousemove', e => {
+        cursor.setAttribute("style", "top: " + (e.pageY - 10) + "px; left: " + (e.pageX - 10) + "px;");
+        });
+    
+        document.addEventListener('click', () => {
+        cursor.classList.add("expand");
+    
+        setTimeout(() => {
+        cursor.classList.remove("expand");
+            }, 500);
+    });
+    </script>
+      
+  </section><!-- End Contact Section -->
+
+  <div class="credits">
+    Designed by <a href="https://www.mertdemirtez.com/">Mert Demir</a>
+  </div>
+
+  <!-- Vendor JS Files -->
+  <script src="assets/vendor/purecounter/purecounter_vanilla.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <script src="assets/vendor/glightbox/js/glightbox.min.js"></script>
+  <script src="assets/vendor/isotope-layout/isotope.pkgd.min.js"></script>
+  <script src="assets/vendor/swiper/swiper-bundle.min.js"></script>
+  <script src="assets/vendor/waypoints/noframework.waypoints.js"></script>
+  <script src="assets/vendor/php-email-form/validate.js"></script>
+
+  <!-- Template Main JS File -->
+  <script src="assets/js/main.js"></script>
+
+</body>
+
+</html>
